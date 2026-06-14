603d07fe001542f2dedda8ebee2f2b6a22c0c1e6
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-06-14 20:38:01 +0530

    add routing for auth pages

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router";
+import { Signup } from "./pages/Signup";
+import { Login } from "./pages/Login";
+import { Homepage } from "./pages/Homepage";
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage></Homepage>}></Route>
+      <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/signup" element={<Signup></Signup>}></Route>
+    </Routes>
+  );
+}
+
+export default App;
