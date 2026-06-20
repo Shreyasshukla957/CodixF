9c5a5c51a07d47f69bf26fcc38cfb61f02218234
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-06-20 23:29:28 +0530

    Setup app routes with public, protected, and admin route guards

--- App.tsx
+++ App.tsx
@@ -2,14 +2,44 @@ import { Routes, Route } from "react-router";
 import { Signup } from "./pages/Signup";
 import { Login } from "./pages/Login";
 import { Homepage } from "./pages/Homepage";
-
+import { PublicRoute, ProtectedRoute } from "./components/protectedRoute";
+import { useDispatch } from "react-redux";
+import type { AppDispatch } from "@/store/store";
+import { useEffect } from "react";
+import { fetchUser } from "@/features/authSlice";
+import AdminRoute from "./pages/admin/Adminroute";
+import CreateProblemPage from "./pages/admin/create-problem";
+import MEditor from "./components/codeEditor";
 
 function App() {
+  // const {isAuthenticated} = useSelector((state:any)=>state.auth)
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+  console.log("hii");
+
+  // const user = useSelector((state:any)=> state.auth.user);
+  // console.log(user);
+  // const role = user.role;
+
   return (
     <Routes>
-      <Route path="/" element={<Homepage></Homepage>}></Route>
-      <Route path="/login" element={<Login></Login>}></Route>
-      <Route path="/signup" element={<Signup></Signup>}></Route>
+      <Route element={<PublicRoute />}>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
+      </Route>
+
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Homepage></Homepage>}></Route>
+        <Route path="/problem-page" element={<MEditor/>}></Route>
+      </Route>
+
+      <Route element={<AdminRoute />}>
+        {" "}
+        <Route path="/admin" element={<CreateProblemPage />}></Route>
+      </Route>
     </Routes>
   );
 }
